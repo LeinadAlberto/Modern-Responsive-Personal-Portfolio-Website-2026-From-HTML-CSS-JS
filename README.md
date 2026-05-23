cb8e89f4e7eee49050d7a7fef4bb0236b73d6052
Alberto <dany.canaviri2022@gmail.com>
2026-05-22 23:55:14 -0400

    The initial structure of the project was created

---
+++ README.md
@@ -0,0 +1,12 @@
+# Responsive Portfolio Website Bianca
+
+![preview img](/preview.png) 
+
+## 📌 Project Features
+- Fully responsive portfolio website Bianca (Mobile First Methodology).
+- Clear and semantic HTML structure.
+- Use of CSS variables and modern JavaScript functions.
+- Smooth scrolling between sections.
+- Scroll animations.
+- Compatible with all modern browsers and devices.
+
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*=============== SHOW & CLOSE MENU ===============*/
+
+
+/* Show menu */
+
+
+/* Hide menu */
+
+
+/*=============== REMOVE MOBILE MENU ===============*/
+
+
+/*=============== HOME TEXT CIRCULAR ===============*/
+
+
+/*=============== HOME TYPED JS ===============*/
+
+
+/*=============== CHANGE HEADER STYLES ===============*/
+
+
+/*=============== SWIPER WORK ===============*/ 
+
+
+/*=============== SERVICES ACCORDION ===============*/ 
+
+
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/ 
+
+
+/*=============== CONTACT EMAIL JS ===============*/ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== CUSTOM CURSOR ===============*/
+
+
+/*=============== SCROLLREVEAL ANIMATION ===============*/
