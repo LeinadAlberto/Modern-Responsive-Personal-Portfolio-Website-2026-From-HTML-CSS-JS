1cf6251f91c1869a30446da448642f364e3fc725
Alberto <dany.canaviri2022@gmail.com>
2026-06-01 23:58:43 -0400

    Integration of swiper.js in the works section

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,22 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER WORK ===============*/
+const swiperWork = new Swiper('.work__swiper', {
+	loop: true, 
+	spaceBetween: 24, 
+	slidesPerView: 'auto', 
+	grabCursor: true, 
+	speed: 600,
 
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	}
+})
 
 /*=============== SERVICES ACCORDION ===============*/
 
